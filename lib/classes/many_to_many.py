54e8aa42bf7711acc834c2de3440698c575a13ce
Buddha Lo <lo_977@yahoo.com>
2025-01-17 23:03:06 -0800

    complete

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,40 +1,76 @@
 class NationalPark:
+    all = []
 
     def __init__(self, name):
         self.name = name
+        NationalPark.all.append(self)
+    @property
+    def name(self):
+        return self._name 
+    @name.setter
+    def name(self,name):
+        if not hasattr(self,"_name"):
+            self._name = name
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.national_park == self]
     
     def visitors(self):
-        pass
+        return list({trip.visitor for trip in self.trips()})
     
     def total_visits(self):
-        pass
+        return len(self.trips())
     
     def best_visitor(self):
-        pass
+        visits = [trip.visitor for trip in self.trips()]
+        return max(visits, key=lambda x: visits.count(x))
 
 
 class Trip:
-    
+    all = []
     def __init__(self, visitor, national_park, start_date, end_date):
         self.visitor = visitor
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+        Trip.all.append(self)
+    @property
+    def start_date(self):
+        return self._start_date
+    
+    @start_date.setter
+    def start_date(self, start_date):
+        if isinstance(start_date, str) and len(start_date) >= 7:
+            self._start_date = start_date
 
+    @property
+    def end_date(self):
+        return self._end_date
+    @end_date.setter
+    def end_date(self,end_date):
+        if isinstance(end_date,str) and len(end_date) >= 7:
+            self._end_date = end_date
 
+        
 class Visitor:
+    all = []
 
     def __init__(self, name):
         self.name = name
+        Visitor.all.append(self)
+    @property
+    def name(self):
+        return self._name 
+    @name.setter
+    def name(self,name):
+        if isinstance(name,str) and 1 <= len(name) <= 15:
+            self._name = name
         
     def trips(self):
-        pass
+        return [trip for trip in Trip.all if trip.visitor == self]
     
     def national_parks(self):
-        pass
+        return list({trip.national_park for trip in self.trips()})
     
     def total_visits_at_park(self, park):
-        pass
+        return sum(1 for trip in self.trips() if trip.national_park == park)
